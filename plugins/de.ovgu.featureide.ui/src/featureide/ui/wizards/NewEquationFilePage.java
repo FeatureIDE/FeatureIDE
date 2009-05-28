4971a7fc48909c7c35be5a847882d086b91add1f
Christian Becker <christian.becker@st.ovgu.de>
2009-05-28 19:11:46 +0000

    Add a Browse Button to the Wizard
    git-svn-id: https://faracvs.cs.uni-magdeburg.de/svn/tthuem-FeatureIDE/trunk@83 272fd0a3-3bb5-487d-978a-b927a3a9602e

--- plugins/de.ovgu.featureide.ui/src/featureide/ui/wizards/NewEquationFilePage.java
+++ plugins/de.ovgu.featureide.ui/src/featureide/ui/wizards/NewEquationFilePage.java
@@ -20,6 +20,9 @@ package featureide.ui.wizards;
 
 import org.eclipse.core.resources.IContainer;
 import org.eclipse.core.resources.IResource;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.core.runtime.Path;
 import org.eclipse.jface.dialogs.IDialogPage;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.IStructuredSelection;
@@ -27,11 +30,15 @@ import org.eclipse.jface.wizard.WizardPage;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.ModifyEvent;
 import org.eclipse.swt.events.ModifyListener;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Text;
+import org.eclipse.ui.dialogs.ContainerSelectionDialog;
 
 import featureide.core.CorePlugin;
 import featureide.core.IFeatureProject;
@@ -40,10 +47,12 @@ import featureide.core.IFeatureProject;
  * The "New" wizard page allows setting the container for the new file as well
  * as the file name. The page will only accept file name without the extension
  * OR with the extension that matches the expected one (jak).
+ * 
+ * @author Christian Becker
  */
 
 public class NewEquationFilePage extends WizardPage {
-//	private Text containerText;
+	private Text containerText;
 
 	private Text fileText;
 
@@ -74,24 +83,24 @@ public class NewEquationFilePage extends WizardPage {
 		layout.numColumns = 3;
 		layout.verticalSpacing = 9;
 		Label label = new Label(container, SWT.NULL);
-//		label.setText("&Container:");
+		label.setText("&Container:");
 //
-//		containerText = new Text(container, SWT.BORDER | SWT.SINGLE);
+		containerText = new Text(container, SWT.BORDER | SWT.SINGLE);
 		GridData gd = new GridData(GridData.FILL_HORIZONTAL);
-//		containerText.setLayoutData(gd);
-//		containerText.addModifyListener(new ModifyListener() {
-//			public void modifyText(ModifyEvent e) {
-//				dialogChanged();
-//			}
-//		});
-//
-//		Button button = new Button(container, SWT.PUSH);
-//		button.setText("Browse...");
-//		button.addSelectionListener(new SelectionAdapter() {
-//			public void widgetSelected(SelectionEvent e) {
-//				handleBrowse();
-//			}
-//		});
+		containerText.setLayoutData(gd);
+		containerText.addModifyListener(new ModifyListener() {
+			public void modifyText(ModifyEvent e) {
+				dialogChanged();
+			}
+		});
+
+		Button button = new Button(container, SWT.PUSH);
+		button.setText("Browse...");
+		button.addSelectionListener(new SelectionAdapter() {
+			public void widgetSelected(SelectionEvent e) {
+				handleBrowse();
+			}
+		});
 		label = new Label(container, SWT.NULL);
 		label.setText("&File name:");
 
@@ -133,7 +142,7 @@ public class NewEquationFilePage extends WizardPage {
 					container = null;
 				}
 					
-//				containerText.setText(container.getFullPath().toString());
+				containerText.setText(container.getFullPath().toString());
 			}
 		}
 		fileText.setText("NewEquation.equation");
@@ -144,17 +153,34 @@ public class NewEquationFilePage extends WizardPage {
 	 * the container field.
 	 */
 
-//	private void handleBrowse() {
-//		ContainerSelectionDialog dialog = new ContainerSelectionDialog(
-//				getShell(), ResourcesPlugin.getWorkspace().getRoot(), false,
-//				"Select new file container");
-//		if (dialog.open() == ContainerSelectionDialog.OK) {
-//			Object[] result = dialog.getResult();
-//			if (result.length == 1) {
-//				containerText.setText(((Path) result[0]).toString());
-//			}
-//		}
-//	}
+	private void handleBrowse() {
+		IPath path;
+		IResource res;
+		ContainerSelectionDialog dialog = new ContainerSelectionDialog(
+				getShell(), ResourcesPlugin.getWorkspace().getRoot(), false,
+				"Select new file container");
+		if (dialog.open() == ContainerSelectionDialog.OK) {
+			Object[] result = dialog.getResult();
+			if (result.length == 1) {
+				if(result[0] instanceof Path){
+					path=(IPath) result[0];
+					containerText.setText(((Path) result[0]).toString());
+					res = ResourcesPlugin.getWorkspace().getRoot().findMember(path);
+					IFeatureProject data = CorePlugin.getProjectData(res);
+					if(data!=null){
+						container=data.getEquationFolder();	
+						
+					}
+					else{
+						container=null;
+					}
+					dialogChanged();
+					
+				}
+								
+			}
+		}
+	}
 
 	/**
 	 * Ensures that both text fields are set.
@@ -164,14 +190,14 @@ public class NewEquationFilePage extends WizardPage {
 //		IResource container = ResourcesPlugin.getWorkspace().getRoot()
 //				.findMember(new Path(getContainerName()));
 		String fileName = getFileName();
-
-//		if (getContainerName().length() == 0) {
+		
+//		if (container==null ) {
 //			updateStatus("File container must be specified");
 //			return;
 //		}
 		if (container == null
 				|| (container.getType() & (IResource.PROJECT | IResource.FOLDER)) == 0) {
-			updateStatus("File container must be a Jak Project");
+			updateStatus("File container must be specified or a Jak Project");
 			return;
 		}
 		if (!container.isAccessible()) {
