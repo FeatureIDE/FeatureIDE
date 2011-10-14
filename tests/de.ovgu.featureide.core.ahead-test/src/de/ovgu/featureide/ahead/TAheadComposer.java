b35a21595c45f996db4a974b9b6229d9ce2802de
Jens Meinicke <jens.meinicke@st.ovgu.de>
2011-10-14 11:53:05 +0000

    Some Tests for the Ahead composer
    git-svn-id: https://faracvs.cs.uni-magdeburg.de/svn/tthuem-FeatureIDE/trunk@1473 272fd0a3-3bb5-487d-978a-b927a3a9602e

---
+++ tests/de.ovgu.featureide.core.ahead-test/src/de/ovgu/featureide/ahead/TAheadComposer.java
@@ -0,0 +1,78 @@
+/* FeatureIDE - An IDE to support feature-oriented software development
+ * Copyright (C) 2005-2011  FeatureIDE Team, University of Magdeburg
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see http://www.gnu.org/licenses/.
+ *
+ * See http://www.fosd.de/featureide/ for further information.
+ */
+package de.ovgu.featureide.ahead;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+/**
+ * Tests for the <code>Ahead</code> composer.
+ * 
+ * @author Jens Meinicke
+ */
+public class TAheadComposer {
+
+	public static final String SOUCE_1 = "layer test;\r\n"
+			+ "import import1;\r\n" + "public class test {\r\n"
+			+ "private Player player;"
+			+ "	private void method1() {\r\n" + "\r\n" + "	}\r\n" + "}";
+	private static final String CORRECTED_1 = "\r\n" + "import import1;\r\n"
+			+ "public class test {\r\n"
+			+ "private Player player;"
+			+ "	private void method1() {\r\n"
+			+ "\r\n" + "	}\r\n" + "}";
+
+	/**
+	 * Tests layer removing.
+	 */
+	@Test
+	public void correctFileTextTest_1() {
+		assertEquals(AheadComposer.correctFileText(SOUCE_1), CORRECTED_1);
+	}
+
+	private static final String SOUCE_2 = "import import1;\r\n"
+			+ "public class test {\r\n" + "	private void method1() {\r\n"
+			+ "\r\n" + "	}\r\n" + "}";
+	private static final String CORRECTED_2 = "\r\n" + "import import1;\r\n"
+			+ "public class test {\r\n" + "	private void method1() {\r\n"
+			+ "\r\n" + "	}\r\n" + "}";
+
+	/**
+	 * Tests adding a line brake before import;
+	 */
+	@Test
+	public void correctFileTextTest_2() {
+		assertEquals(AheadComposer.correctFileText(SOUCE_2), CORRECTED_2);
+	}
+
+	private static final String SOUCE_3 = "\r\n" + "import player.*;\r\n"
+			+ "public class Player {\r\n" + "	private void player() {\r\n"
+			+ "\r\n" + "	}\r\n"
+			+ "private Player player;\r\n"
+			+ "}";
+
+	/**
+	 * Tests ignoring other layer occurrences.
+	 */
+	@Test
+	public void correctFileTextTest_3() {
+		assertEquals(AheadComposer.correctFileText(SOUCE_3), null);
+	}
+}
