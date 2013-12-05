a674e439236b64a7b816202eccc0d82864d76a3d
Christoph Giesel <mail@cgiesel.de>
2013-12-05 19:16:37 +0100

    multi software product line plugin created

---
+++ plugins/de.ovgu.featureide.core.mspl/src/de/ovgu/featureide/core/mspl/MSPLPlugin.java
@@ -0,0 +1,50 @@
+package de.ovgu.featureide.core.mspl;
+
+import org.eclipse.ui.plugin.AbstractUIPlugin;
+import org.osgi.framework.BundleContext;
+
+/**
+ * The activator class controls the plug-in life cycle
+ */
+public class MSPLPlugin extends AbstractUIPlugin {
+
+	// The plug-in ID
+	public static final String PLUGIN_ID = "de.ovgu.featureide.core.mspl"; //$NON-NLS-1$
+
+	// The shared instance
+	private static MSPLPlugin plugin;
+	
+	/**
+	 * The constructor
+	 */
+	public MSPLPlugin() {
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext)
+	 */
+	public void start(BundleContext context) throws Exception {
+		super.start(context);
+		plugin = this;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext)
+	 */
+	public void stop(BundleContext context) throws Exception {
+		plugin = null;
+		super.stop(context);
+	}
+
+	/**
+	 * Returns the shared instance
+	 *
+	 * @return the shared instance
+	 */
+	public static MSPLPlugin getDefault() {
+		return plugin;
+	}
+
+}
