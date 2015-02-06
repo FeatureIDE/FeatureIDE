264e6e94baabb3dca47e99ea788722112a843257
kTonscheidt <ktonscheidt@web.de>
2015-02-06 15:17:26 +0100

    plugin migration moved to correct folder name

---
+++ plugins/de.ovgu.featureide.migration/src/de/ovgu/featureide/migration/impl/SPLMigrationUtils.java
@@ -0,0 +1,237 @@
+/* FeatureIDE - A Framework for Feature-Oriented Software Development
+ * Copyright (C) 2005-2015  FeatureIDE team, University of Magdeburg, Germany
+ *
+ * This file is part of FeatureIDE.
+ * 
+ * FeatureIDE is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * FeatureIDE is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with FeatureIDE.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * See http://featureide.cs.ovgu.de/ for further information.
+ */
+package de.ovgu.featureide.migration.impl;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
+
+import org.eclipse.core.resources.IContainer;
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IFolder;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.core.runtime.Path;
+
+import de.ovgu.featureide.fm.core.FeatureModel;
+import de.ovgu.featureide.fm.core.io.FeatureModelWriterIFileWrapper;
+import de.ovgu.featureide.fm.core.io.xml.XmlFeatureModelWriter;
+
+/**
+ * This class implements methods that might be useful in Migrating a Set of
+ * Projects into a FeatureIDE Project. Currently this is only implemented for
+ * the FeatureHouse composer in {@link VariantsToFeatureHouseSPLMigrator}.
+ * 
+ * @author Konstantin Tonscheidt
+ * 
+ */
+public class SPLMigrationUtils
+{
+	/**
+	 * Copies all folders and files from {@code source} to {@code destination}.
+	 * 
+	 * @param source
+	 * @param destination
+	 * @throws CoreException
+	 */
+	public static void recursiveCopyFiles(IContainer source, IContainer destination)
+			throws CoreException
+	{
+		final IResource[] members = source.members();
+		for (int i = 0; i < members.length; i++)
+		{
+			final IResource member = members[i];
+			final IPath currentPath = new Path(member.getName());
+
+			if (member instanceof IContainer)
+			{
+				final IFolder subFolder = destination.getFolder(currentPath);
+				if (!subFolder.exists())
+				{
+					member.copy(subFolder.getFullPath(), true, null);
+					recursiveCopyFiles((IContainer) member, subFolder);
+				}
+			} else
+				if (member instanceof IFile)
+				{
+					final IFile copyFile = destination.getFile(currentPath);
+					if (!copyFile.exists())
+						member.copy(copyFile.getFullPath(), true, null);
+				} else
+					assert false : "Only expected  Files and Containers to copy";
+		}
+	}
+
+	/**
+	 * creates a new Folder in {@code project} at the given {@code path}.
+	 * 
+	 * @param project
+	 *            the project, the folder is going to be created in.
+	 * @param path
+	 *            a path relative to the project root.
+	 */
+	public static void createFolderInProject(IProject project, IPath path)
+	{
+		if (path == null || path.isEmpty())
+			return;
+
+		IFolder newFolder = project.getFolder(path);
+		if (newFolder.exists())
+		{
+			assert false : "Trying to create an already existing folder: " + path;
+			System.out.println("Folder " + path + " already exists");
+			return;
+		}
+		try
+		{
+			newFolder.create(true, true, null);
+			project.refreshLocal(IProject.DEPTH_INFINITE, null);
+			System.out.println("Creation of Folder " + path + " successful");
+		} catch (CoreException e)
+		{
+			System.out.println("Creation of Folder " + path + " lead to CoreException:"
+					+ e.getMessage());
+			e.printStackTrace();
+		}
+		if (!newFolder.exists())
+			System.out.println("Folder " + path + " does not exist after creation");
+
+	}
+
+	/**
+	 * convenience method for creating folders from a path saved in a
+	 * {@link String}.
+	 * 
+	 * @param project
+	 *            the project, the folder is going to be created in.
+	 * @param path
+	 *            a path relative to the project root.
+	 * 
+	 * @see {@link #createFolderInProject(IProject, IPath)}
+	 */
+	public static void createFolderInProject(IProject project, String path)
+	{
+		IPath newPath = new Path(path);
+		System.out.println("Creating Folder at " + path + " in project " + project.getName());
+		createFolderInProject(project, newPath);
+	}
+
+	/**
+	 * Tries to create a new Project in the Workspace and returns it.
+	 * 
+	 * @param projectName
+	 * @return the new {@link IProject} if successful, null if not.
+	 */
+	public static IProject createProject(String projectName)
+	{
+		IProject newProject = ResourcesPlugin.getWorkspace().getRoot().getProject(projectName);
+		if (newProject.exists())
+			throw new IllegalArgumentException("Cannot create project " + projectName
+					+ " because it already exists.");
+
+		try
+		{
+			newProject.create(null);
+		} catch (CoreException e)
+		{
+			e.printStackTrace();
+			return null;
+		}
+
+		return newProject;
+
+	}
+
+	/**
+	 * Creates a {@code projectName}.config file containing {@code projectName}
+	 * in the projects config folder.
+	 * 
+	 * @param project
+	 * @param configPath
+	 * @param projectName
+	 * @throws CoreException
+	 * @throws UnsupportedEncodingException
+	 */
+	public static void createConfigFile(IProject project, String configPath, String projectName)
+			throws CoreException, UnsupportedEncodingException
+	{
+		final IFolder configFolder = project.getFolder(configPath);
+		final IFile configFile = configFolder.getFile(projectName + ".config");
+		InputStream defaultContent = new ByteArrayInputStream(projectName.getBytes("UTF-8"));
+
+		configFile.create(defaultContent, true, null);
+
+	}
+
+	/**
+	 * Writes the {@code featureModel} to the default location (
+	 * {@code /model.xml}) in {@code featureProject}
+	 * 
+	 * @param featureProject
+	 * @param featureModel
+	 */
+	public static void writeFeatureModelToDefaultFile(IProject featureProject,
+			FeatureModel featureModel)
+	{
+		FeatureModelWriterIFileWrapper fmWriter = new FeatureModelWriterIFileWrapper(
+				new XmlFeatureModelWriter(featureModel));
+		IFile featureModelFile = featureProject.getFile("model.xml");
+
+		assert featureModelFile.exists() : "model.xml does not exist";
+
+		try
+		{
+			fmWriter.writeToFile(featureModelFile);
+		} catch (CoreException e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Creates the Folder empty. If it exists, it will be deleted and created
+	 * new.
+	 * 
+	 * @param folder
+	 *            the folder to be created.
+	 * @return the created {@link IFolder}s {@link IPath}, or null if creation
+	 *         was not successful
+	 */
+	public static IPath setupFolder(IFolder folder)
+	{
+		try
+		{
+			if (folder.exists())
+				folder.delete(true, null);
+
+			folder.create(true, true, null);
+		} catch (CoreException e)
+		{
+			e.printStackTrace();
+		}
+
+		return folder.exists() ? folder.getFullPath() : null;
+	}
+}
