1451f16a32ede1b1d472b64d5b49c354bbc1c81a
skrieter <Sebastian.Krieter@st.ovgu.de>
2015-03-01 22:08:43 +0100

    First implementation of calculating feature model graph.

--- plugins/de.ovgu.featureide.fm.core/src/de/ovgu/featureide/fm/core/FeatureModel.java
+++ plugins/de.ovgu.featureide.fm.core/src/de/ovgu/featureide/fm/core/FeatureModel.java
@@ -22,8 +22,10 @@ package de.ovgu.featureide.fm.core;
 
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.Hashtable;
 import java.util.LinkedList;
 import java.util.List;
@@ -35,7 +37,12 @@ import javax.annotation.CheckForNull;
 import javax.annotation.Nonnull;
 
 import org.eclipse.core.resources.IProject;
+import org.prop4j.Implies;
+import org.prop4j.Literal;
 import org.prop4j.Node;
+
+import de.ovgu.featureide.fm.core.job.AStoppableJob;
+import de.ovgu.featureide.fm.core.job.IJob;
 
 /**
  * The model representation of the feature tree that notifies listeners of
@@ -881,6 +888,479 @@ public class FeatureModel extends DeprecatedFeatureModel implements PropertyCons
 			hash = hash * 7 + c.toString().hashCode();
 		}
 		return hash;
+	}
+	
+	public void splitModel() {
+		final IJob splittJob = new AStoppableJob("Spliting Feature Model") {
+			private Collection<Feature> coreFeatures = null;
+			private FeatureGraph featureGraph = null;
+			
+			@Override
+			protected boolean work() throws Exception {
+				System.out.println("Computing...");
+				coreFeatures = getAnalyser().getCoreFeatures();
+				final List<Constraint> constraints = getConstraints();
+				final Collection<Feature> features = new LinkedList<Feature>(getFeatures());
+				features.removeAll(coreFeatures);
+				
+				workMonitor.setMaxAbsoluteWork(2*features.size() + 2);
+				
+				featureGraph = new FeatureGraph(features);
+				
+				workMonitor.worked();
+				
+				for (Feature feature : features) {
+					if (!coreFeatures.contains(feature.getParent())) {
+						featureGraph.implies(feature.getName(), feature.getParent().getName());
+						if (feature.getParent().isAnd() && feature.isMandatory()) {
+							featureGraph.implies(feature.getParent().getName(), feature.getName());
+						} else if (feature.getParent().isAlternative() || feature.getParent().isOr()) {
+							featureGraph.setEdge(feature.getParent().getName(), feature.getName(), FeatureGraph.EDGE_1q);
+							featureGraph.setEdge(feature.getName(), feature.getParent().getName(), FeatureGraph.EDGE_0q);
+						} else if (feature.getParent().isAnd() && !feature.isMandatory()) {
+							featureGraph.setEdge(feature.getName(), feature.getParent().getName(), FeatureGraph.EDGE_0q);
+						}
+					}
+					if (feature.getParent().isAlternative()) {
+						for (Feature sibiling : feature.getParent().getChildren()) {
+							if (!coreFeatures.contains(sibiling)) {
+								featureGraph.setEdge(feature.getName(), sibiling.getName(), FeatureGraph.EDGE_10);
+								featureGraph.setEdge(feature.getName(), sibiling.getName(), FeatureGraph.EDGE_0q);
+							}
+						}
+					} else if (feature.getParent().isOr()) {
+						boolean optionalFeature = false;
+						for (Feature sibiling : feature.getParent().getChildren()) {
+							if (coreFeatures.contains(sibiling)) {
+								optionalFeature = true;
+								break;
+							}
+						}
+						if (!optionalFeature) {
+							for (Feature sibiling : feature.getParent().getChildren()) {
+								featureGraph.setEdge(feature.getName(), sibiling.getName(), FeatureGraph.EDGE_0q);
+							}
+						}
+					}
+				}
+				
+				for (Constraint constraint : constraints) {
+					final Collection<Feature> containedFeatures = constraint.getContainedFeatures();
+					containedFeatures.removeAll(coreFeatures);
+					final Node node = constraint.getNode();
+					if (!specialConnect(node)) {
+						for (Feature feature : containedFeatures) {
+							for (Feature feature2 : containedFeatures) {
+								featureGraph.setEdge(feature.getName(), feature2.getName(), FeatureGraph.EDGE_0q);
+								featureGraph.setEdge(feature.getName(), feature2.getName(), FeatureGraph.EDGE_1q);
+							}
+						}
+					}
+				}
+				
+				featureGraph.clearDiagonal();
+				
+				workMonitor.worked();
+				
+				for (Feature feature : features) {
+					featureGraph.dfs(new byte[features.size()], featureGraph.featureMap.get(feature.getName()), true);
+					workMonitor.worked();
+				}
+				
+				for (Feature feature : features) {
+					featureGraph.dfs(new byte[features.size()], featureGraph.featureMap.get(feature.getName()), false);
+					workMonitor.worked();
+				}
+				
+				final int[] featureNeigbors = new int[features.size()];
+				int i = 0;
+				for (Feature feature : features) {
+					featureNeigbors[i++] = featureGraph.countNeighbors(feature.getName());
+				}
+				
+				final int[] featureNeigbors2 = new int[features.size()];
+				i = 0;
+				for (Feature feature : features) {
+					featureNeigbors2[i++] = featureGraph.countNeighbors2(feature.getName());
+				}
+
+				Arrays.sort(featureNeigbors);
+				for (int j = 0; j < featureNeigbors.length; j++) {
+					System.out.print(featureNeigbors[j] + ", ");
+				}
+				System.out.println();
+				
+				Arrays.sort(featureNeigbors2);
+				for (int j = 0; j < featureNeigbors2.length; j++) {
+					System.out.print(featureNeigbors2[j] + ", ");
+				}
+				System.out.println();
+				
+				return true;
+			}
+			
+			private boolean specialConnect(Node constraintNode) { 
+				if (constraintNode instanceof Implies) {
+					final Node leftNode = constraintNode.getChildren()[0];
+					final Node rightNode = constraintNode.getChildren()[1];
+					if (leftNode instanceof Literal) {
+						if (rightNode instanceof Literal) {
+							Feature impliedFeature = getFeature((String) ((Literal) rightNode).var);
+							if (!coreFeatures.contains(impliedFeature)) {
+								featureGraph.implies((String) ((Literal) leftNode).var, (String) ((Literal) rightNode).var);
+							}
+							return true;
+						}
+//						} else if (rightNode instanceof And) {
+//							for (Node impliedNode : rightNode.getChildren()) {
+//								
+//								Feature impliedFeature = getFeature(impliedNode);
+//								if (!coreFeatures.contains(impliedFeature)) {
+//									implies((String) ((Literal) leftNode).var, impliedNode);
+//								}
+//							}
+//							return true;
+//						}
+//					} else if (leftNode instanceof Or) {
+//						if (rightNode instanceof Literal) {
+//							for (Node implyNode : leftNode.getChildren()) {
+//								
+//							}
+//							return true;
+//						} else if (rightNode instanceof And) {
+//							for (Node implyNode : leftNode.getChildren()) {
+//								for (Node impliedNode : rightNode.getChildren()) {
+//									
+//								}
+//							}
+//							return true;
+//						}
+					}
+				}
+				return false;
+			}
+		};
+		splittJob.schedule();
+	}
+	
+	private static class FeatureGraph {
+		public static final byte
+			EDGE_NONE = 0b00000000,
+			EDGE_11 = 0b00000100, //0x04,
+			EDGE_10 = 0b00001000, //0x08,
+			EDGE_1q = 0b00001100, //0x0c,
+			EDGE_01 = 0b00010000, //0x10,
+			EDGE_00 = 0b00100000, //0x20,
+			EDGE_0q = 0b00110000; //0x30;
+		
+		public static final byte
+			MASK_1_11110011 = (byte) 0b11110011, //0xf3,
+			MASK_0_11001111 = (byte) 0b11001111, //0xcf,
+			MASK_1_00001100 = ~MASK_1_11110011,
+			MASK_0_00110000 = ~MASK_0_11001111;
+		
+		public final String[] featureArray;
+		
+		private final byte[] adjMatrix;
+		private final int size;
+		private final HashMap<String, Integer> featureMap; 
+		
+		public FeatureGraph(Collection<Feature> features) {
+			size = features.size();
+			featureMap = new HashMap<>(size << 1);
+			featureArray = new String[size];
+			
+			int i = 0;
+			for (Feature feature : features) {
+				featureArray[i++] = feature.getName();
+			}
+			Arrays.sort(featureArray);
+			for (int j = 0; j < featureArray.length; j++) {
+				featureMap.put(featureArray[j], j);
+			}
+			
+			adjMatrix = new byte[size * size];
+		}
+		
+		private void implies(String implyFeature, String impliedFeature) {
+			setEdge(implyFeature, impliedFeature, FeatureGraph.EDGE_11);
+			setEdge(impliedFeature, implyFeature, FeatureGraph.EDGE_00);
+		}
+
+		public void setEdge(String from, String to, byte edgeType) {
+			setEdge(featureMap.get(from), featureMap.get(to), edgeType);
+		}
+		
+		public void setEdge(int from, int to, byte edgeType) {
+			final int index = (from * size) + to;
+			final byte oldValue = adjMatrix[index];
+			final int newValue;
+			switch (edgeType) {
+			case EDGE_NONE:
+				newValue = EDGE_NONE;
+				break;
+			case EDGE_0q:
+				switch (oldValue & MASK_0_00110000) {
+				case EDGE_NONE:
+					newValue = oldValue | edgeType | 2;
+					break;
+				default:
+					newValue = oldValue;
+				}
+				break;
+			case EDGE_00: case EDGE_01: 
+				switch (oldValue & MASK_0_00110000) {
+					case EDGE_NONE:
+						newValue = oldValue | edgeType | 2;
+						break;
+					case EDGE_0q:
+						newValue = (oldValue & MASK_0_11001111) | edgeType;
+						break;
+					default:
+						newValue = oldValue | EDGE_0q;
+				}
+				break;
+			case EDGE_1q:
+				switch (oldValue & MASK_1_00001100) {
+				case EDGE_NONE:
+					newValue = oldValue | edgeType | 1;
+					break;
+				default:
+					newValue = oldValue;
+				}
+				break;
+			case EDGE_10: case EDGE_11: 
+				switch (oldValue & MASK_1_00001100) {
+					case EDGE_NONE:
+						newValue = oldValue | edgeType | 1;
+						break;
+					case EDGE_1q:
+						newValue = (oldValue & MASK_1_11110011) | edgeType;
+						break;
+					default:
+						newValue = oldValue | EDGE_1q;
+				}
+				break;
+			default:
+				newValue = EDGE_NONE;
+			}
+			adjMatrix[index] = (byte) newValue;
+		}
+		
+//		public byte getEdge(String from, String to) {
+//			return getEdge(featureMap.get(from), featureMap.get(to));
+//		}
+		
+		public byte getEdge(int fromIndex, int toIndex) {			
+			return adjMatrix[(fromIndex * size) + toIndex];
+		}
+		
+		public void clearDiagonal() {
+			for (int i = 0; i < adjMatrix.length; i+=(size + 1)) {
+				adjMatrix[i] = EDGE_NONE;
+			}
+		}
+		
+		public int countNeighbors(String from) {
+			final int fromIndex = featureMap.get(from);
+			
+			int count = 0;
+			for (int i = (fromIndex * size), end = i + size; i < end; i++) {
+				count += adjMatrix[i] % 2;
+			}
+			
+			return count;
+		}
+		
+		public int countNeighbors2(String from) {
+			final int fromIndex = featureMap.get(from);
+			
+			int count = 0;
+			for (int i = (fromIndex * size), end = i + size; i < end; i++) {
+				count += (adjMatrix[i] >>> 1) % 2;
+			}
+			
+			return count;
+		}
+		
+		// visited: 0 not visited, 1 visited (unknown status), 2 visited (known status)
+		private void dfs(byte[] visited, int curFeature, boolean selected) {
+			System.out.println(featureArray[curFeature] + " " + selected);
+			visited[curFeature] = 2;
+			
+			for (int j = 0; j < visited.length; j++) {
+				final byte visit = visited[j];
+				if (visit < 2) {
+					final byte childSelected;
+					if (selected) {
+						switch (getEdge(curFeature, j) & MASK_1_00001100) {
+							case EDGE_10:
+								// don't select child
+								childSelected = 0;
+								visited[j] = 2;
+								System.out.println("\tq " + featureArray[j]);
+								break;
+							case EDGE_11:
+								// select child
+								childSelected = 1;
+								visited[j] = 2;
+								System.out.println("\tq " + featureArray[j]);
+								break;
+							case EDGE_1q:
+								// ?
+								if (visit == 1) {
+									continue;
+								}
+								visited[j] = 1;
+								childSelected = 2;
+								System.out.println("\tq " + featureArray[j]);
+								break;
+							default:
+								continue;
+						}
+					} else {
+						switch (getEdge(curFeature, j) & MASK_0_00110000) {
+							case EDGE_00:
+								// don't select child
+								childSelected = 0;
+								visited[j] = 2;
+								System.out.println("\t0 " + featureArray[j]);
+								break;
+							case EDGE_01:
+								// select child
+								childSelected = 1;
+								visited[j] = 2;
+								System.out.println("\t1 " + featureArray[j]);
+								break;
+							case EDGE_0q:
+								// ?
+								if (visit == 1) {
+									continue;
+								}
+								visited[j] = 1;
+								childSelected = 2;
+								System.out.println("\tq " + featureArray[j]);
+								break;
+							default:
+								continue;
+						}
+					}
+					
+					dfs_rec(visited, j, curFeature, childSelected, selected);
+				}
+			}
+		}
+		
+		private void dfs_rec(byte[] visited, int curFeature, int parentFeature, byte selected, boolean parentSelected) {
+			for (int j = 0; j < visited.length; j++) {
+				final byte visit = visited[j];
+				if (visit == 0) {
+					final byte edge = getEdge(curFeature, j);
+					switch (selected) {
+						case 0:
+							switch (edge & MASK_0_00110000) {
+								// visit = 0, not selected, implies not selected
+								case EDGE_00:
+									visited[j] = 2;
+									System.out.println("\t0 " + featureArray[j]);
+									setEdge(parentFeature, j, parentSelected ? EDGE_10 : EDGE_00);
+									dfs_rec(visited, j, parentFeature, (byte)0, parentSelected);
+									break;
+								// visit = 0, not selected, implies selected
+								case EDGE_01:
+									visited[j] = 2;
+									System.out.println("\t1 " + featureArray[j]);
+									setEdge(parentFeature, j, parentSelected ? EDGE_11 : EDGE_01);
+									dfs_rec(visited, j, parentFeature, (byte)1, parentSelected);
+									break;
+								// visit = 0, not selected, implies ?
+								case EDGE_0q:
+									visited[j] = 1;
+									System.out.println("\tq " + featureArray[j]);
+									setEdge(parentFeature, j, parentSelected ? EDGE_1q : EDGE_0q);
+									dfs_rec(visited, j, parentFeature, (byte)2, parentSelected);
+									break;
+								// default ???
+							}
+							break;
+						case 1:
+							switch (edge & MASK_1_00001100) {
+								// visit = 0, selected, implies not selected
+								case EDGE_10:
+									visited[j] = 2;
+									System.out.println("\t0 " + featureArray[j]);
+									setEdge(parentFeature, j, parentSelected ? EDGE_10 : EDGE_00);
+									dfs_rec(visited, j, parentFeature, (byte)0, parentSelected);
+									break;
+								// visit = 0, selected, implies selected
+								case EDGE_11:
+									visited[j] = 2;
+									System.out.println("\t1 " + featureArray[j]);
+									setEdge(parentFeature, j, parentSelected ? EDGE_11 : EDGE_01);
+									dfs_rec(visited, j, parentFeature, (byte)1, parentSelected);
+									break;
+								// visit = 0, selected, implies ?
+								case EDGE_1q:
+									visited[j] = 1;
+									System.out.println("\tq " + featureArray[j]);
+									setEdge(parentFeature, j, parentSelected ? EDGE_1q : EDGE_0q);
+									dfs_rec(visited, j, parentFeature, (byte)2, parentSelected);
+									break;
+								// default ???
+							}
+							break;
+						case 2:
+							if (edge > 0) {
+								visited[j] = 1;
+								System.out.println("\tq " + featureArray[j]);
+								setEdge(parentFeature, j, parentSelected ? EDGE_1q : EDGE_0q);
+								dfs_rec(visited, j, parentFeature, (byte)2, parentSelected);
+							}
+							break;
+					}
+				} else if (visit == 1) {
+					final byte edge = getEdge(curFeature, j);
+					switch (selected) {
+						case 0:
+							switch (edge & MASK_0_00110000) {
+								// visit = 1, not selected, implies not selected
+								case EDGE_00:
+									visited[j] = 2;
+									System.out.println("\t0 " + featureArray[j]);
+									setEdge(parentFeature, j, parentSelected ? EDGE_10 : EDGE_00);
+									dfs_rec(visited, j, parentFeature, (byte)0, parentSelected);
+									break;
+								// visit = 1, not selected, implies selected
+								case EDGE_01:
+									visited[j] = 2;
+									System.out.println("\t1 " + featureArray[j]);
+									setEdge(parentFeature, j, parentSelected ? EDGE_11 : EDGE_01);
+									dfs_rec(visited, j, parentFeature, (byte)1, parentSelected);
+									break;
+								// default ???
+							}
+							break;
+						case 1:
+							switch (edge & MASK_1_00001100) {
+								// visit = 1, selected, implies not selected
+								case EDGE_10:
+									visited[j] = 2;
+									setEdge(parentFeature, j, parentSelected ? EDGE_10 : EDGE_00);
+									dfs_rec(visited, j, parentFeature, (byte)0, parentSelected);
+									break;
+								// visit = 1, selected, implies selected
+								case EDGE_11:
+									visited[j] = 2;
+									setEdge(parentFeature, j, parentSelected ? EDGE_11 : EDGE_01);
+									dfs_rec(visited, j, parentFeature, (byte)1, parentSelected);
+									break;
+								// default ???
+							}
+							break;
+					}
+				}
+			}
+		}
 	}
 
 }
