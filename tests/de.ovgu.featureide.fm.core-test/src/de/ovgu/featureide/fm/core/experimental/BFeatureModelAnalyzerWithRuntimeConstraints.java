6e93d7e4e127438d5656785dac59161576ba2397
Marcus Pinnecke <marcus.pinnecke@gmail.com>
2015-03-24 02:07:54 +0100

    Adapt to real runtimes of test

--- tests/de.ovgu.featureide.fm.core-test/src/de/ovgu/featureide/fm/core/experimental/BFeatureModelAnalyzerWithRuntimeConstraints.java
+++ tests/de.ovgu.featureide.fm.core-test/src/de/ovgu/featureide/fm/core/experimental/BFeatureModelAnalyzerWithRuntimeConstraints.java
@@ -158,7 +158,7 @@ public class BFeatureModelAnalyzerWithRuntimeConstraints {
 	
 	//@Test (timeout=8000) // 0.886s @ i5(3,3GHz)
 	@Record(samples=10)
-	@Constraint(samples=10, allowedMedianDeviation=250)
+	@Constraint(samples=10, allowedMedianDeviation=1200)
 	public final void BAnalyzeFeatureModel200() {
 		analyze(200);
 	}
