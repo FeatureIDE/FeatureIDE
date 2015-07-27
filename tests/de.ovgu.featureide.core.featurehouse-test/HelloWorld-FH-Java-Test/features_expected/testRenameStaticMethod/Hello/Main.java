de8d51902cb08ec6731deee5a3961aa834522bf5
stefschu <steffen1.schulze@st.ovgu.de>
2015-07-27 11:26:44 +0200

    UnitTests for Rename Class and Rename Method

---
+++ tests/de.ovgu.featureide.core.featurehouse-test/HelloWorld-FH-Java-Test/features_expected/testRenameStaticMethod/Hello/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+
+	protected void print() {
+		System.out.print("Hello");
+	}
+	
+	public static void mainStatic(String[] args){
+		new Main().print();
+	}
+	
+	public Main(){
+	}
+	
+	protected void print1() {
+		System.out.print("print1");
+	}
+	
+}
---
+++ tests/de.ovgu.featureide.core.featurehouse-test/HelloWorld-FH-Java-Test/features_expected/testRenameVirtualMethodInPackage/Hello/ovgu/Main.java
@@ -0,0 +1,12 @@
+package ovgu;
+
+public class Main {
+
+	protected void println() {
+		System.out.print("Hello");
+	}
+	
+	public static void main(String[] args){
+		new Main().println();
+	}
+}
