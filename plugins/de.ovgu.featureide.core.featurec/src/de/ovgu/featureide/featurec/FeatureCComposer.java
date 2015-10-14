ad960f9f8f21839bb451ad583d3a780753f19348
Christopher Kruczek <christopher.kruczek@metop.de>
2015-10-14 14:42:11 +0200

    Setup FeatureCCorePlugin/FeatureCComposer

---
+++ plugins/de.ovgu.featureide.core.featurec/src/de/ovgu/featureide/featurec/FeatureCComposer.java
@@ -0,0 +1,25 @@
+package de.ovgu.featureide.featurec;
+
+import org.eclipse.core.resources.IFile;
+
+import de.ovgu.featureide.core.builder.ComposerExtensionClass;
+
+public class FeatureCComposer extends ComposerExtensionClass {
+
+	public FeatureCComposer() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void performFullBuild(IFile config) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Mechanism getGenerationMechanism() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
