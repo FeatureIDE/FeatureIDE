0d1ad35f3dad86023a304c0211a422b131597a35
danielhohmann <dtwin95@gmx.de>
2016-03-14 14:52:46 +0100

    Adding methods for creating jars programmatically Changed loaded resources when creating a new project

--- plugins/de.ovgu.featureide.core.framework/src/de/ovgu/featureide/core/framework/FrameworkComposer.java
+++ plugins/de.ovgu.featureide.core.framework/src/de/ovgu/featureide/core/framework/FrameworkComposer.java
@@ -1,11 +1,19 @@
 package de.ovgu.featureide.core.framework;
 
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.URL;
+import java.nio.file.FileSystems;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.jar.JarOutputStream;
+import java.util.zip.ZipEntry;
 
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IFolder;
@@ -43,10 +51,88 @@ public class FrameworkComposer extends ComposerExtensionClass {
 	public Mechanism getGenerationMechanism() {
 		return null;
 	}
-	
+
 	@Override
 	public void copyNotComposedFiles(Configuration c, IFolder destination) {
 		/** Should do nothing, otherwise will copy jar files to src folder **/
+	}
+
+	/**
+	 * Creates JARs from all project in "resources" folder inside the main
+	 * projects
+	 * 
+	 * @return <code>false</code> if creation was not successful
+	 */
+	private boolean createJARs(IProject project) {
+		IFolder resources = project.getFolder("projects");
+		IFolder features = featureProject.getSourceFolder();
+
+		try {
+			for (IResource res : resources.members()) {
+				if (res instanceof IFolder && selectedFeatures.contains(res.getName())) {
+					String jarName = res.getName() + ".jar";
+					IFile infoFile = ((IFolder) res).getFile("info.xml");
+					if (!FrameworkValidator.validate(infoFile)) {
+						return false;
+					}
+
+					IFolder bin = ((IFolder) res).getFolder("bin");
+					final String path = features.getLocation()
+							.append(res.getName() + FileSystems.getDefault().getSeparator() + jarName).toString();
+					try (FileOutputStream fileStream = new FileOutputStream(path);
+							JarOutputStream jarStream = new JarOutputStream(fileStream)) {
+						addToJAR(jarStream, bin);
+						addFileToJAR(jarStream, infoFile, "");
+					} catch (IOException e) {
+						FrameworkCorePlugin.getDefault().logError(e);
+						return false;
+					}
+				}
+			}
+		} catch (CoreException e) {
+			FrameworkCorePlugin.getDefault().logError(e);
+			return false;
+		}
+		return true;
+	}
+
+	private void addFileToJAR(JarOutputStream jarStream, IFile res, String path) throws IOException {
+		jarStream.putNextEntry(
+				new ZipEntry(path + res.getName()));
+		URL location = FileLocator.toFileURL(res.getLocationURI().toURL());
+		File file = new File(location.getPath());
+		try (BufferedInputStream in = new BufferedInputStream(new FileInputStream(file))) {
+			byte[] buffer = new byte[1024];
+			while (true) {
+				int count = in.read(buffer);
+				if (count == -1) {
+					break;
+				}
+				jarStream.write(buffer, 0, count);
+			}
+		}
+	}
+
+	private void addToJAR(JarOutputStream jarStream, IResource bin) throws CoreException, IOException {
+		if (bin instanceof IFolder) {
+			for (IResource member : ((IFolder) bin).members()) {
+				addToJAR(jarStream, member, "");
+			}
+		}
+	}
+
+	private void addToJAR(JarOutputStream jarStream, IResource res, String path) throws IOException, CoreException {
+		if (res instanceof IFolder) {
+			String path2 = path + res.getName()
+					+ FileSystems.getDefault().getSeparator();
+			jarStream.putNextEntry(new ZipEntry(path2));
+			for (IResource member : ((IFolder) res).members()) {
+				addToJAR(jarStream, member, path2);
+			}
+		}
+		else if (res instanceof IFile) {
+			addFileToJAR(jarStream, (IFile) res, path);
+		}
 	}
 
 	@Override
@@ -65,7 +151,27 @@ public class FrameworkComposer extends ComposerExtensionClass {
 		}
 
 		IProject project = config.getProject();
+		try {
+			project.deleteMarkers("de.ovgu.featureide.core.featureModuleMarker", true, IResource.DEPTH_ZERO);
+		} catch (CoreException e) {
+			FrameworkCorePlugin.getDefault().logError(e);
+		}
+		if (!createJARs(project)) {
+			FrameworkCorePlugin.getDefault().logWarning("JARs not build");
+			// try {
+			// IMarker marker =
+			// project.createMarker("de.ovgu.featureide.core.featureModuleMarker");
+			// marker.setAttribute(IMarker.MESSAGE, "A problem occured while
+			// building JARs");
+			// marker.setAttribute(IMarker.SEVERITY, IMarker.SEVERITY_ERROR);
+			// } catch (CoreException e) {
+			// FrameworkCorePlugin.getDefault().logError(e);
+			// }
+		}
+		;
+
 		setBuildpaths(project);
+
 		try {
 			project.refreshLocal(IResource.DEPTH_INFINITE, null);
 			featureProject.getProject().build(IncrementalProjectBuilder.FULL_BUILD, null);
@@ -212,27 +318,29 @@ public class FrameworkComposer extends ComposerExtensionClass {
 			}
 		}
 
-		/** Create config file **/
-		try {
-			inputStream = FileLocator.openStream(FrameworkCorePlugin.getDefault().getBundle(),
-					new Path("resources/config.txt"), false);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		file = folder.getFile("config.txt");
-		if (!file.exists()) {
-			try {
-				file.create(inputStream, true, null);
-			} catch (CoreException e) {
-				FrameworkCorePlugin.getDefault().logError(e);
-			}
-		}
-
-		try {
-			featureProject.getBuildFolder().refreshLocal(IResource.DEPTH_INFINITE, null);
-		} catch (CoreException e) {
-			FrameworkCorePlugin.getDefault().logError(e);
-		}
+		// /** Create config file **/
+		// try {
+		// inputStream =
+		// FileLocator.openStream(FrameworkCorePlugin.getDefault().getBundle(),
+		// new Path("resources/config.txt"), false);
+		// } catch (IOException e) {
+		// e.printStackTrace();
+		// }
+		// file = folder.getFile("config.txt");
+		// if (!file.exists()) {
+		// try {
+		// file.create(inputStream, true, null);
+		// } catch (CoreException e) {
+		// FrameworkCorePlugin.getDefault().logError(e);
+		// }
+		// }
+		//
+		// try {
+		// featureProject.getBuildFolder().refreshLocal(IResource.DEPTH_INFINITE,
+		// null);
+		// } catch (CoreException e) {
+		// FrameworkCorePlugin.getDefault().logError(e);
+		// }
 
 		return true;
 
