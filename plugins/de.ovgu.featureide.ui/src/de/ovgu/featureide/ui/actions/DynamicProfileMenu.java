f1a48428a9889b86f3ee05c66e977f623ec283d4
Reimar Schröter <reimar.schroeter@ovgu.de>
2016-07-15 09:40:32 +0200

    fix nightly build
    restored compatibility to build server eclipse version

--- plugins/de.ovgu.featureide.ui/src/de/ovgu/featureide/ui/actions/DynamicProfileMenu.java
+++ plugins/de.ovgu.featureide.ui/src/de/ovgu/featureide/ui/actions/DynamicProfileMenu.java
@@ -158,7 +158,7 @@ public class DynamicProfileMenu extends ContributionItem {
 		} else if (element instanceof IJavaElement) {
 			return CorePlugin.getFeatureProject(((IJavaElement) element).getJavaProject().getProject());
 		} else if (element instanceof IAdaptable) {
-			final IProject project = ((IAdaptable) element).getAdapter(IProject.class);
+			final IProject project = (IProject)((IAdaptable) element).getAdapter(IProject.class);
 			if (project != null) {
 				return CorePlugin.getFeatureProject(project);
 			}
