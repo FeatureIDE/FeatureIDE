a03ccb489113fce99dbe928639a064290b986beb
Jens Meinicke <jensmeinicke@gmx.de>
2016-12-06 15:54:53 -0500

    add test for fuji, currently ignored

--- tests/de.ovgu.featureide.core.featurehouse-test/src/de/ovgu/featureide/featurehouse/jar/FHJavaVersionTest.java
+++ tests/de.ovgu.featureide.core.featurehouse-test/src/de/ovgu/featureide/featurehouse/jar/FHJavaVersionTest.java
@@ -1,19 +1,26 @@
 package de.ovgu.featureide.featurehouse.jar;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 import composer.FSTGenComposer;
+import fuji.Main;
 import de.ovgu.featureide.fm.core.jar.JavaVersionTest;
 
 /**
- * Checks the Java Version of FeatureHouse.jar
+ * Checks the Java Version of FeatureHouse jars
  * 
  * @author Jens Meinicke
  */
 public class FHJavaVersionTest {
 
 	@Test
-	public void testJavaVersion() throws Exception {
+	public void testFeatureHouseJavaVersion() throws Exception {
 		JavaVersionTest.testJavaVersion(FSTGenComposer.class);
+	}
+	
+	@Test@Ignore
+	public void testFUJIJavaVersion() throws Exception {
+		JavaVersionTest.testJavaVersion(Main.class);
 	}
 }
