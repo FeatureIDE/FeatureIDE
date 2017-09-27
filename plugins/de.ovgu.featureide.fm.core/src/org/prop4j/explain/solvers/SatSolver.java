53917bae15b0e8b931cd43400fefea17cfeaf832
Timo Günther <y0058127@tu-bs.de>
2017-09-27 16:13:49 +0200

    Avoid pre tags in JavaDocs

--- plugins/de.ovgu.featureide.fm.core/src/org/prop4j/explain/solvers/SatSolver.java
+++ plugins/de.ovgu.featureide.fm.core/src/org/prop4j/explain/solvers/SatSolver.java
@@ -44,10 +44,13 @@ public interface SatSolver extends SatProblem {
 	 *
 	 * <p> More formally, let <i>f</i> denote the current (possibly empty) problem. Those are all the clauses that have been {@link #addFormula(Node) added} to
 	 * the problem. <i>f</i> is a conjunction of <i>n</i> &ge; 0 CNF clauses <i>f<sub>i</sub></i>. Each clause <i>f<sub>i</sub></i> in turn consists of a
-	 * positive amount of possibly negated literals <i>f<sub>ij</sub></i>: <pre><i>f</i> = <i>f<sub>1</sub></i> &and; &hellip; &and; <i>f<sub>n</sub></i> =
-	 * (<i>f<sub>11</sub></i> &or; &hellip; &or; <i>f<sub>1m</sub></i>) &and; &hellip; &and; (<i>f<sub>n1</sub></i> &or; &hellip; &or;
-	 * <i>f<sub>nk</sub></i>)</pre> </p> Then, <i>f</i> is satisfiable iff there is a variable assignment that satisfies <i>f</i> (i.e. makes it evaluate to
-	 * true). Such an assignment is called {@link #getModel() model}. </p>
+	 * positive amount of possibly negated literals <i>f<sub>ij</sub></i>: </p>
+	 *
+	 * <p> <blockquote><i>f</i> = <i>f<sub>1</sub></i> &and; &hellip; &and; <i>f<sub>n</sub></i><br> &nbsp; = (<i>f<sub>11</sub></i> &or; &hellip; &or;
+	 * <i>f<sub>1m</sub></i>) &and; &hellip; &and; (<i>f<sub>n1</sub></i> &or; &hellip; &or; <i>f<sub>nk</sub></i>)</blockquote> </p>
+	 *
+	 * </p> Then, <i>f</i> is satisfiable iff there is a variable assignment that satisfies <i>f</i> (i.e. makes it evaluate to true). Such an assignment is
+	 * called {@link #getModel() model}. </p>
 	 *
 	 * @return whether the problem is satisfiable
 	 */
