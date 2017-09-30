cfb5262a6f800ad7ca525a341a4415dd8fc8f6e1
Timo Günther <y0058127@tu-bs.de>
2017-09-30 16:05:33 +0200

    Revert "Fix formula"
    This reverts commit 5f90da8393cabbc7ce53dd4fdee39248c36e6901.

--- plugins/de.ovgu.featureide.fm.core/src/de/ovgu/featureide/fm/core/editing/FeatureModelToNodeTraceModel.java
+++ plugins/de.ovgu.featureide.fm.core/src/de/ovgu/featureide/fm/core/editing/FeatureModelToNodeTraceModel.java
@@ -27,6 +27,7 @@ import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Set;
 
+import org.prop4j.And;
 import org.prop4j.Implies;
 import org.prop4j.Literal;
 import org.prop4j.Node;
@@ -225,7 +226,7 @@ public class FeatureModelToNodeTraceModel implements Cloneable {
 			case CHILD_DOWN:
 				return new Implies(getLiteral(), new Or(getLiterals()));
 			case CHILD_HORIZONTAL:
-				return new Not(new Or(getLiterals()));
+				return new Not(new And(getLiterals()));
 			case ROOT:
 				return getLiteral();
 			case CONSTRAINT:
