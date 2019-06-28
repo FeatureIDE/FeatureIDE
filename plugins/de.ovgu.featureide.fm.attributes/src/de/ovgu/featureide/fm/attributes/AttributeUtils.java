13fb9c7b18885c465520f33893ee0966c946ebe4
Thomas Thuem <t.thuem@tu-braunschweig.de>
2019-06-28 23:17:21 +0200

    Removed code that uses Java 8

--- plugins/de.ovgu.featureide.fm.attributes/src/de/ovgu/featureide/fm/attributes/AttributeUtils.java
+++ plugins/de.ovgu.featureide.fm.attributes/src/de/ovgu/featureide/fm/attributes/AttributeUtils.java
@@ -1,9 +1,5 @@
 package de.ovgu.featureide.fm.attributes;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
-
 import de.ovgu.featureide.fm.attributes.base.IFeatureAttribute;
 import de.ovgu.featureide.fm.attributes.base.impl.ExtendedFeature;
 import de.ovgu.featureide.fm.attributes.base.impl.ExtendedFeatureModel;
@@ -12,96 +8,95 @@ import de.ovgu.featureide.fm.core.base.IFeature;
 import de.ovgu.featureide.fm.core.base.IFeatureModel;
 
 public class AttributeUtils {
-	
-	
-	public static String[] getNumericalAttributes(IFeatureModel featureModel) {
-		if(featureModel instanceof ExtendedFeatureModel) {
-			ExtendedFeatureModel extModel = (ExtendedFeatureModel) featureModel;
-			List<String> numericalAttributeNames = new ArrayList<>();
-			for (IFeature feat : extModel.getFeatures()) {
-				ExtendedFeature ext = (ExtendedFeature) feat;
-				for (IFeatureAttribute att: ext.getAttributes()) {
-					if(isNumerical(att)) {
-						numericalAttributeNames.add(att.getName());
-					}
-				}
-			}
-			List<Object> namesWithoutDuplicates = numericalAttributeNames.stream().distinct().collect(Collectors.toList());
-			return namesWithoutDuplicates.toArray(new String[namesWithoutDuplicates.size()]);
-		}
-		
-		return new String[] {"Not an extended Feature Model"};
-		
-	}
-	
-	public static String[] getBooleanAttributes(IFeatureModel featureModel) {
-		if(featureModel instanceof ExtendedFeatureModel) {
-			ExtendedFeatureModel extModel = (ExtendedFeatureModel) featureModel;
-			List<String> booleanAttributeNames = new ArrayList<>();
-			for (IFeature feat : extModel.getFeatures()) {
-				ExtendedFeature ext = (ExtendedFeature) feat;
-				for (IFeatureAttribute att: ext.getAttributes()) {
-					if(isBoolean(att)) {
-						booleanAttributeNames.add(att.getName());
-					}
-				}
-			}
-			List<Object> namesWithoutDuplicates = booleanAttributeNames.stream().distinct().collect(Collectors.toList()); // TODO: Java 8
-			return namesWithoutDuplicates.toArray(new String[namesWithoutDuplicates.size()]);
-		}
-		
-		return new String[] {"Not an extended Feature Model"};
-	}
-	
+
+//	public static String[] getNumericalAttributes(IFeatureModel featureModel) {
+//		if (featureModel instanceof ExtendedFeatureModel) {
+//			ExtendedFeatureModel extModel = (ExtendedFeatureModel) featureModel;
+//			List<String> numericalAttributeNames = new ArrayList<>();
+//			for (IFeature feat : extModel.getFeatures()) {
+//				ExtendedFeature ext = (ExtendedFeature) feat;
+//				for (IFeatureAttribute att : ext.getAttributes()) {
+//					if (isNumerical(att)) {
+//						numericalAttributeNames.add(att.getName());
+//					}
+//				}
+//			}
+//			List<Object> namesWithoutDuplicates = numericalAttributeNames.stream().distinct().collect(Collectors.toList()); // TODO: remove Java 8
+//			return namesWithoutDuplicates.toArray(new String[namesWithoutDuplicates.size()]);
+//		}
+//
+//		return new String[] { "Not an extended Feature Model" };
+//
+//	}
+
+//	public static String[] getBooleanAttributes(IFeatureModel featureModel) {
+//		if (featureModel instanceof ExtendedFeatureModel) {
+//			ExtendedFeatureModel extModel = (ExtendedFeatureModel) featureModel;
+//			List<String> booleanAttributeNames = new ArrayList<>();
+//			for (IFeature feat : extModel.getFeatures()) {
+//				ExtendedFeature ext = (ExtendedFeature) feat;
+//				for (IFeatureAttribute att : ext.getAttributes()) {
+//					if (isBoolean(att)) {
+//						booleanAttributeNames.add(att.getName());
+//					}
+//				}
+//			}
+//			List<Object> namesWithoutDuplicates = booleanAttributeNames.stream().distinct().collect(Collectors.toList()); // TODO: remove Java 8
+//			return namesWithoutDuplicates.toArray(new String[namesWithoutDuplicates.size()]);
+//		}
+//
+//		return new String[] { "Not an extended Feature Model" };
+//	}
+
 	public static boolean isNumerical(IFeatureAttribute att) {
 		return att.getType().equals(FeatureAttribute.LONG) || att.getType().equals(FeatureAttribute.DOUBLE);
 	}
-	
+
 	public static boolean isBoolean(IFeatureAttribute att) {
 		return att.getType().equals(FeatureAttribute.BOOLEAN);
 	}
-	
+
 	public static String getUnitByName(IFeatureModel featureModel, String attribute) {
 		if (attribute == null) {
 			return null;
 		}
-		if(featureModel instanceof ExtendedFeatureModel) {
+		if (featureModel instanceof ExtendedFeatureModel) {
 			ExtendedFeatureModel extModel = (ExtendedFeatureModel) featureModel;
 			for (IFeature feat : extModel.getFeatures()) {
 				ExtendedFeature ext = (ExtendedFeature) feat;
-				for (IFeatureAttribute att: ext.getAttributes()) {
+				for (IFeatureAttribute att : ext.getAttributes()) {
 					if (att.getName().equals(attribute)) {
 						return att.getUnit();
 					}
 				}
 			}
 		}
-		
+
 		return null;
 	}
-	
+
 	public static Double getDoubleValue(IFeatureAttribute att, Double defaultValue) {
-		if(!isNumerical(att)) {
+		if (!isNumerical(att)) {
 			return null;
 		}
 		Object value = att.getValue();
 		if (value == null) {
 			return defaultValue;
 		}
-		if(value instanceof Long) {
+		if (value instanceof Long) {
 			return ((Long) value).doubleValue();
 		} else {
 			return (double) value;
 		}
 	}
-	
+
 	public static Boolean getBooleanValue(IFeatureAttribute att) {
 		if (!isBoolean(att) || att.getValue() == null) {
 			return null;
 		}
 		return (boolean) att.getValue();
 	}
-	
+
 	public static Double getBooleanValueAsDouble(IFeatureAttribute att, Double defaultValue) {
 		Double trueDouble = 1d;
 		Double falseDouble = 0d;
@@ -111,5 +106,5 @@ public class AttributeUtils {
 		}
 		return value ? trueDouble : falseDouble;
 	}
-	
+
 }
