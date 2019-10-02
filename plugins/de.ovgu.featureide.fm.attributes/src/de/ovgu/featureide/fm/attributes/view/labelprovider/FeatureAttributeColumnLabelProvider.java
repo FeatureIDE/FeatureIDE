f5817e2802e02fd46d550f3c8d3fcd198be4dd39
Subaro <joshua.sprey@web.de>
2019-10-02 13:04:33 +0200

    Fixed #906: The tooltips for all features are wrong when the feature model contains a dead/FA feature

--- plugins/de.ovgu.featureide.fm.attributes/src/de/ovgu/featureide/fm/attributes/view/labelprovider/FeatureAttributeColumnLabelProvider.java
+++ plugins/de.ovgu.featureide.fm.attributes/src/de/ovgu/featureide/fm/attributes/view/labelprovider/FeatureAttributeColumnLabelProvider.java
@@ -30,10 +30,13 @@ import org.eclipse.swt.graphics.Point;
 
 import de.ovgu.featureide.fm.attributes.base.IFeatureAttribute;
 import de.ovgu.featureide.fm.attributes.view.FeatureAttributeView;
+import de.ovgu.featureide.fm.core.AnalysesCollection;
+import de.ovgu.featureide.fm.core.analysis.cnf.formula.FeatureModelFormula;
 import de.ovgu.featureide.fm.core.base.IFeature;
 import de.ovgu.featureide.fm.core.color.ColorPalette;
 import de.ovgu.featureide.fm.core.color.FeatureColor;
 import de.ovgu.featureide.fm.core.color.FeatureColorManager;
+import de.ovgu.featureide.fm.core.io.manager.FeatureModelManager;
 
 /**
  * Abstract label provider that is subclassed by every label provider of the {@link FeatureAttributeView}.
@@ -72,7 +75,9 @@ public abstract class FeatureAttributeColumnLabelProvider extends ColumnLabelPro
 	public String getToolTipText(Object element) {
 		if (element instanceof IFeature) {
 			IFeature feature = (IFeature) element;
-			return feature.createTooltip(new Object[0]);
+			final FeatureModelFormula variableFormula = FeatureModelManager.getInstance(feature.getFeatureModel()).getVariableFormula();
+			final AnalysesCollection properties = variableFormula.getAnalyzer().getAnalysesCollection();
+			return feature.createTooltip(properties);
 		} else {
 			return null;
 		}
@@ -85,13 +90,13 @@ public abstract class FeatureAttributeColumnLabelProvider extends ColumnLabelPro
 
 	@Override
 	public int getToolTipDisplayDelayTime(Object object) {
-		// Dsiplay tooltip after 1000 ms (1sek)
+		// Display tooltip after 1000 ms (1sek)
 		return 1000;
 	}
 
 	@Override
 	public int getToolTipTimeDisplayed(Object object) {
-		// Dsiplay tooltip for 15000 ms (15sek)
+		// Display tooltip for 15000 ms (15sek)
 		return 15000;
 	}
 }
