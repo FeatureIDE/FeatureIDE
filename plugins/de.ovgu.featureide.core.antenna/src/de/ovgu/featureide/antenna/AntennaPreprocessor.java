1aceee15f091efbff0b8512b25cb126881894c53
Sebastian Krieter <sebastian.krieter@ovgu.de>
2019-11-01 12:27:13 +0100

    Fixed problem in Antenna plug-in.
    Adapted usage of node reader to new API.

--- plugins/de.ovgu.featureide.core.antenna/src/de/ovgu/featureide/antenna/AntennaPreprocessor.java
+++ plugins/de.ovgu.featureide.core.antenna/src/de/ovgu/featureide/antenna/AntennaPreprocessor.java
@@ -54,6 +54,7 @@ import org.eclipse.core.runtime.Status;
 import org.eclipse.core.runtime.jobs.Job;
 import org.prop4j.Literal;
 import org.prop4j.Node;
+import org.prop4j.NodeReader.ErrorHandling;
 import org.prop4j.Not;
 
 import antenna.preprocessor.v3.PPException;
@@ -92,8 +93,8 @@ public class AntennaPreprocessor extends PPComposerExtensionClass {
 
 	public AntennaPreprocessor() {
 		super(ANTENNA);
-		nodereader.setIgnoreMissingFeatures(true);
-		nodereader.setIgnoreUnparsableSubExpressions(true);
+		nodereader.setIgnoreMissingFeatures(ErrorHandling.KEEP);
+		nodereader.setIgnoreUnparsableSubExpressions(ErrorHandling.KEEP);
 	}
 
 	@Override
