1964b3aa3031b6c89ac5dc69592444ea19c57100
Dominik Engelhardt <dominikengelhardt@ymail.com>
2020-05-30 16:58:03 +0200

    Implement printing

--- plugins/de.ovgu.featureide.fm.core/src/de/ovgu/featureide/fm/core/io/uvl/UVLFeatureModelFormat.java
+++ plugins/de.ovgu.featureide.fm.core/src/de/ovgu/featureide/fm/core/io/uvl/UVLFeatureModelFormat.java
@@ -23,7 +23,9 @@ package de.ovgu.featureide.fm.core.io.uvl;
 import java.io.File;
 import java.nio.file.Path;
 import java.util.List;
+import java.util.Map;
 import java.util.Objects;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -44,8 +46,10 @@ import de.neominik.uvl.ast.Or;
 import de.neominik.uvl.ast.ParseError;
 import de.neominik.uvl.ast.UVLModel;
 import de.ovgu.featureide.fm.core.PluginID;
+import de.ovgu.featureide.fm.core.base.IConstraint;
 import de.ovgu.featureide.fm.core.base.IFeature;
 import de.ovgu.featureide.fm.core.base.IFeatureModel;
+import de.ovgu.featureide.fm.core.base.IFeatureStructure;
 import de.ovgu.featureide.fm.core.base.impl.FMFactoryManager;
 import de.ovgu.featureide.fm.core.base.impl.MultiFeatureModel;
 import de.ovgu.featureide.fm.core.base.impl.MultiFeatureModelFactory;
@@ -65,6 +69,9 @@ public class UVLFeatureModelFormat extends AFeatureModelFormat {
 
 	public static final String ID = PluginID.PLUGIN_ID + ".format.fm." + UVLFeatureModelFormat.class.getSimpleName();
 	public static final String FILE_EXTENSION = "uvl";
+
+	private static final String NS_ATTRIBUTE_NAME = "namespace";
+	private static final String NS_ATTRIBUTE_FEATURE = "_synthetic_ns_feature";
 
 	private UVLModel rootModel;
 
@@ -124,6 +131,7 @@ public class UVLFeatureModelFormat extends AFeatureModelFormat {
 		fm.getStructure().setRoot(root.getStructure());
 		List.of(rootModel.getConstraints()).forEach(c -> parseConstraint(fm, c));
 		List.of(rootModel.getImports()).forEach(i -> parseImport(fm, i));
+		fm.addAttribute(NS_ATTRIBUTE_FEATURE, NS_ATTRIBUTE_NAME, rootModel.getNamespace());
 	}
 
 	private IFeature parseFeature(MultiFeatureModel fm, IFeature root, Feature f) {
@@ -134,6 +142,7 @@ public class UVLFeatureModelFormat extends AFeatureModelFormat {
 		}
 		feature.getStructure().setAbstract(isAbstract(resolved));
 		List.of(resolved.getGroups()).forEach(g -> parseGroup(fm, feature, g));
+		parseAttributes(resolved, fm);
 		return feature;
 	}
 
@@ -158,8 +167,25 @@ public class UVLFeatureModelFormat extends AFeatureModelFormat {
 		return Objects.equals(true, f.getAttributes().get("abstract"));
 	}
 
+	@SuppressWarnings("unchecked")
+	private void parseAttributes(Feature f, MultiFeatureModel fm) {
+		((Map<String, Object>) f.getAttributes()).entrySet().stream().filter(e -> e.getKey().equals("constraint") || e.getKey().equals("constraints"))
+				.forEach(e -> parseAttribute(fm, e.getValue()));
+	}
+
+	private void parseAttribute(MultiFeatureModel fm, Object value) {
+		if (value instanceof List<?>) {
+			((List<?>) value).forEach(v -> parseConstraint(fm, v));
+		} else {
+			parseConstraint(fm, value);
+		}
+	}
+
 	private void parseConstraint(IFeatureModel fm, Object c) {
-		fm.addConstraint(MultiFeatureModelFactory.getInstance().createConstraint(fm, parseConstraint(c)));
+		final Node constraint = parseConstraint(c);
+		if (constraint != null) {
+			fm.addConstraint(MultiFeatureModelFactory.getInstance().createConstraint(fm, constraint));
+		}
 	}
 
 	private Node parseConstraint(Object c) {
@@ -187,25 +213,75 @@ public class UVLFeatureModelFormat extends AFeatureModelFormat {
 	 * @param error a {@link ParseError}
 	 * @return a {@link Problem}
 	 */
-	@SuppressWarnings("unchecked")
 	private Problem toProblem(ParseError error) {
-		final StringBuilder sb = new StringBuilder();
-		sb.append("Parse error at line ");
-		sb.append(error.getLine());
-		sb.append(", column ");
-		sb.append(error.getColumn());
-		sb.append(":\n");
-		sb.append(error.getText());
-		if (!error.getExpected().isEmpty()) {
-			sb.append(error.getExpected().stream().collect(Collectors.joining("\n", "\nExpected one of:\n", "\n")));
-		}
-		return new Problem(sb.toString(), error.getLine(), Severity.ERROR);
+		return new Problem(error.toString(), error.getLine(), Severity.ERROR);
 	}
 
 	@Override
 	public String write(IFeatureModel object) {
-		// TODO implement
-		return super.write(object);
+		return deconstructFeatureModel((MultiFeatureModel) object).toString();
+	}
+
+	private UVLModel deconstructFeatureModel(MultiFeatureModel fm) {
+		final UVLModel model = new UVLModel();
+		model.setNamespace(fm.getStringAttributes().getAttribute(NS_ATTRIBUTE_FEATURE, NS_ATTRIBUTE_NAME).getValue());
+		model.setImports(fm.getExternalModels().values().stream().map(um -> new Import(um.getVarName(), um.getModelName())).toArray(Import[]::new));
+		model.setRootFeatures(new Feature[] { printFeature(fm.getStructure().getRoot().getFeature()) });
+		model.setConstraints(fm.getConstraints().stream().map(this::printConstraint).toArray());
+		return model;
+	}
+
+	private Feature printFeature(IFeature feature) {
+		final Feature f = new Feature();
+		f.setName(feature.getName());
+		if (feature.getStructure().isAbstract()) {
+			f.setAttributes(Map.of("abstract", true));
+		}
+		f.setGroups(printGroups(feature));
+		return f;
+	}
+
+	private Group constructGroup(IFeatureStructure fs, String type, Predicate<IFeatureStructure> pred) {
+		return new Group(type, fs.getChildren().stream().filter(f -> !f.getFeature().getName().contains(".")).filter(pred)
+				.map(f -> printFeature(f.getFeature())).toArray(Feature[]::new));
+	}
+
+	private Group[] printGroups(IFeature feature) {
+		final IFeatureStructure fs = feature.getStructure();
+		if (!fs.hasChildren()) {
+			return new Group[] {};
+		}
+		if (fs.isAnd()) {
+			final Group m = constructGroup(fs, "mandatory", c -> c.isMandatory());
+			final Group o = constructGroup(fs, "optional", c -> !c.isMandatory());
+			return Stream.of(m, o).filter(g -> g.getChildren().length > 0).toArray(Group[]::new);
+		} else if (fs.isOr()) {
+			return new Group[] { constructGroup(fs, "or", x -> true) };
+		} else if (fs.isAlternative()) {
+			return new Group[] { constructGroup(fs, "alternative", x -> true) };
+		}
+		return new Group[] {};
+	}
+
+	private Object printConstraint(IConstraint constraint) {
+		return printConstraint(constraint.getNode());
+	}
+
+	private Object printConstraint(Node n) {
+		if (n instanceof Literal) {
+			return ((Literal) n).var;
+		} else if (n instanceof org.prop4j.Not) {
+			return new Not(printConstraint(n.getChildren()[0]));
+		} else if (n instanceof org.prop4j.And) {
+			return new And(printConstraint(n.getChildren()[0]), printConstraint(n.getChildren()[1]));
+		} else if (n instanceof org.prop4j.Or) {
+			return new Or(printConstraint(n.getChildren()[0]), printConstraint(n.getChildren()[1]));
+		} else if (n instanceof Implies) {
+			return new Impl(printConstraint(n.getChildren()[0]), printConstraint(n.getChildren()[1]));
+		} else if (n instanceof Equals) {
+			return new Equiv(printConstraint(n.getChildren()[0]), printConstraint(n.getChildren()[1]));
+		}
+		return null;
 	}
 
 	@Override
@@ -215,8 +291,7 @@ public class UVLFeatureModelFormat extends AFeatureModelFormat {
 
 	@Override
 	public boolean supportsWrite() {
-		// TODO change when implemented
-		return false;
+		return true;
 	}
 
 	@Override
