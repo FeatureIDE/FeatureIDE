cc6458617505d56cd0bc1d3cc6e83ffc8d9874d9
PaulWestphal <paul.aloys.westphal@outlook.de>
2020-09-11 15:01:32 +0200

    Partial feature project derivation is now executed as a job.

---
+++ plugins/de.ovgu.featureide.core/src/de/ovgu/featureide/core/job/PartialFeatureProjectJob.java
@@ -0,0 +1,84 @@
+/* FeatureIDE - A Framework for Feature-Oriented Software Development
+ * Copyright (C) 2005-2017  FeatureIDE team, University of Magdeburg, Germany
+ *
+ * This file is part of FeatureIDE.
+ *
+ * FeatureIDE is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * FeatureIDE is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with FeatureIDE.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * See http://featureide.cs.ovgu.de/ for further information.
+ */
+package de.ovgu.featureide.core.job;
+
+import java.nio.file.Paths;
+
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.CoreException;
+
+import de.ovgu.featureide.core.CorePlugin;
+import de.ovgu.featureide.core.IFeatureProject;
+import de.ovgu.featureide.core.builder.PartialFeatureProjectBuilder;
+import de.ovgu.featureide.fm.core.configuration.Configuration;
+import de.ovgu.featureide.fm.core.job.LongRunningMethod;
+import de.ovgu.featureide.fm.core.job.LongRunningWrapper;
+import de.ovgu.featureide.fm.core.job.monitor.IMonitor;
+
+/**
+ * This copies a feature project, waits for the copy to be registered, and then runs the Partial Feature Project builder to build a partial feature project.
+ *
+ * @author Paul Westphal
+ */
+public class PartialFeatureProjectJob implements LongRunningMethod<Boolean> {
+
+	private final IFile file;
+
+	public PartialFeatureProjectJob(IFile file) {
+		this.file = file;
+	}
+
+	private String getConfigName(IFile file) {
+		String configName = file.getName();
+		final int pos = configName.lastIndexOf(".");
+		if (pos > 0) {
+			configName = configName.substring(0, pos);
+		}
+		return configName;
+	}
+
+	@Override
+	public Boolean execute(IMonitor<Boolean> monitor) throws Exception {
+		final IFeatureProject baseProject = CorePlugin.getFeatureProject(file);
+		final String newProjectName = baseProject.getProjectName() + "_" + getConfigName(file);
+
+		try {
+			baseProject.getProject().copy(baseProject.getProject().getFullPath().removeLastSegments(1).append(newProjectName), true, null);
+		} catch (final CoreException e) {
+			e.printStackTrace();
+		}
+
+		final IProject newProject = ResourcesPlugin.getWorkspace().getRoot().getProject(newProjectName);
+		CorePlugin.getDefault().addProject(newProject);
+
+		IFeatureProject newFeatureProject = CorePlugin.getFeatureProject(newProject);
+		while ((newFeatureProject = CorePlugin.getFeatureProject(newProject)) == null) {}
+		final Configuration config =
+			baseProject.loadConfiguration(Paths.get(ResourcesPlugin.getWorkspace().getRoot().getLocation().toString() + file.getFullPath()));
+
+		System.out.println("check");
+		LongRunningWrapper.runMethod(new PartialFeatureProjectBuilder(newFeatureProject, config));
+		return null;
+	}
+
+}
--- plugins/de.ovgu.featureide.ui/src/de/ovgu/featureide/ui/handlers/BuildPartialProjectHandler.java
+++ plugins/de.ovgu.featureide.ui/src/de/ovgu/featureide/ui/handlers/BuildPartialProjectHandler.java
@@ -22,18 +22,12 @@ package de.ovgu.featureide.ui.handlers;
 
 import static de.ovgu.featureide.fm.core.localization.StringTable.CANT_DERIVE_PARTIAL_PROJECT_BECAUSE_IT_DOES_NOT_BELONG_TO_A_FEATURE_PROJECT;
 
-import java.nio.file.Paths;
-
 import org.eclipse.core.resources.IFile;
-import org.eclipse.core.resources.IProject;
-import org.eclipse.core.resources.ResourcesPlugin;
-import org.eclipse.core.runtime.CoreException;
 
 import de.ovgu.featureide.core.CorePlugin;
 import de.ovgu.featureide.core.IFeatureProject;
-import de.ovgu.featureide.core.builder.PartialFeatureProjectBuilder;
-import de.ovgu.featureide.fm.core.configuration.Configuration;
-import de.ovgu.featureide.fm.core.job.LongRunningWrapper;
+import de.ovgu.featureide.core.job.PartialFeatureProjectJob;
+import de.ovgu.featureide.fm.core.job.LongRunningJob;
 import de.ovgu.featureide.fm.ui.handlers.base.AFileHandler;
 import de.ovgu.featureide.ui.UIPlugin;
 
@@ -50,33 +44,9 @@ public class BuildPartialProjectHandler extends AFileHandler {
 		if (project == null) {
 			UIPlugin.getDefault().logWarning(CANT_DERIVE_PARTIAL_PROJECT_BECAUSE_IT_DOES_NOT_BELONG_TO_A_FEATURE_PROJECT);
 		} else {
-			final IFeatureProject baseProject = CorePlugin.getFeatureProject(file);
-
-			final String newProjectName = baseProject.getProjectName() + "_" + getConfigName(file);
-
-			try {
-				baseProject.getProject().copy(baseProject.getProject().getFullPath().removeLastSegments(1).append(newProjectName), true, null);
-			} catch (final CoreException e) {
-				e.printStackTrace();
-			}
-
-			final IProject newProject = ResourcesPlugin.getWorkspace().getRoot().getProject(newProjectName);
-			IFeatureProject newFeatureProject = CorePlugin.getFeatureProject(newProject);
-			while ((newFeatureProject = CorePlugin.getFeatureProject(newProject)) == null) {}
-			final Configuration config =
-				baseProject.loadConfiguration(Paths.get(ResourcesPlugin.getWorkspace().getRoot().getLocation().toString() + file.getFullPath()));
-
-			System.out.println("check");
-			LongRunningWrapper.runMethod(new PartialFeatureProjectBuilder(newFeatureProject, config));
+			final LongRunningJob<Boolean> job = new LongRunningJob<Boolean>("Creating Partial Feature Project", new PartialFeatureProjectJob(file));
+			job.schedule();
 		}
 	}
 
-	private String getConfigName(IFile file) {
-		String configName = file.getName();
-		final int pos = configName.lastIndexOf(".");
-		if (pos > 0) {
-			configName = configName.substring(0, pos);
-		}
-		return configName;
-	}
 }
