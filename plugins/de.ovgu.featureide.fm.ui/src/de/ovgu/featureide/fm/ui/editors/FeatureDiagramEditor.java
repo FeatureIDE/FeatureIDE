07c8e1faf34fc27de7797329f186536244db5bbf
Benedikt Jutz <benedikt.jutz@uni-ulm.de>
2021-07-23 11:31:32 +0200

    Renamed haveAutoLayout to enableAutoLayoutConstraints.

--- plugins/de.ovgu.featureide.fm.ui/src/de/ovgu/featureide/fm/ui/editors/FeatureDiagramEditor.java
+++ plugins/de.ovgu.featureide.fm.ui/src/de/ovgu/featureide/fm/ui/editors/FeatureDiagramEditor.java
@@ -1157,9 +1157,9 @@ public class FeatureDiagramEditor extends FeatureModelEditorPage implements GUID
 				selectedAction.setChecked(true);
 				menuManager.insert(1, new Separator("ManualLayoutSeparator"));
 				menuManager.insertBefore("ManualLayoutSeparator", autoLayoutConstraintAction);
-				final boolean haveAutoLayout = !graphicalFeatureModel.getLayout().hasFeaturesAutoLayout();
-				autoLayoutConstraintAction.setEnabled(haveAutoLayout);
-				autoLayoutConstraintAction.setChecked(haveAutoLayout && graphicalFeatureModel.getLayout().isAutoLayoutConstraints());
+				final boolean enableAutoLayoutConstraints = !graphicalFeatureModel.getLayout().hasFeaturesAutoLayout();
+				autoLayoutConstraintAction.setEnabled(enableAutoLayoutConstraints);
+				autoLayoutConstraintAction.setChecked(enableAutoLayoutConstraints && graphicalFeatureModel.getLayout().isAutoLayoutConstraints());
 			}
 		});
 		return menuManager;
