efafd830df9075fde250ec6a61502716e1ef27c9
Benedikt Jutz <benedikt.jutz@uni-ulm.de>
2021-08-20 14:05:04 +0200

    Deleting Feature without Cross-Tree-Constraints works.

--- plugins/de.ovgu.featureide.fm.ui/src/de/ovgu/featureide/fm/ui/editors/FeatureDiagramEditor.java
+++ plugins/de.ovgu.featureide.fm.ui/src/de/ovgu/featureide/fm/ui/editors/FeatureDiagramEditor.java
@@ -178,6 +178,7 @@ import de.ovgu.featureide.fm.ui.editors.featuremodel.operations.CreateFeatureAbo
 import de.ovgu.featureide.fm.ui.editors.featuremodel.operations.CreateFeatureOperation;
 import de.ovgu.featureide.fm.ui.editors.featuremodel.operations.CreateGraphicalSiblingOperation;
 import de.ovgu.featureide.fm.ui.editors.featuremodel.operations.DeleteConstraintOperation;
+import de.ovgu.featureide.fm.ui.editors.featuremodel.operations.DeleteFeatureOperation;
 import de.ovgu.featureide.fm.ui.editors.featuremodel.operations.EditConstraintOperation;
 import de.ovgu.featureide.fm.ui.editors.featuremodel.operations.EditConstraintOperation.ConstraintDescription;
 import de.ovgu.featureide.fm.ui.editors.featuremodel.operations.FeatureModelOperationWrapper;
@@ -855,7 +856,6 @@ public class FeatureDiagramEditor extends FeatureModelEditorPage implements GUID
 			viewer.deselectAll();
 			setDirty();
 			analyzeFeatureModel();
-			recentEvents.add(event);
 			break;
 		case MODEL_DATA_SAVED:
 			recentEvents.add(event);
@@ -1092,7 +1092,7 @@ public class FeatureDiagramEditor extends FeatureModelEditorPage implements GUID
 		final FeatureIDEEvent oldEvent = (FeatureIDEEvent) event.getNewValue();
 
 		switch (oldEvent.getEventType()) {
-		case IMPORTED_MODEL_CHANGED:
+		case MULTIPLE_CHANGES_OCCURRED:
 			// Repeat the single changes stored in newValue for the given source.
 			if (oldEvent.getNewValue() instanceof List<?>) {
 				final List<?> singleEvents = (List<?>) oldEvent.getNewValue();
@@ -1103,6 +1103,7 @@ public class FeatureDiagramEditor extends FeatureModelEditorPage implements GUID
 					}
 				}
 			}
+			break;
 		case FEATURE_NAME_CHANGED:
 			// Rerun feature renamings in the imported model, now with modelAlias appended for the new name.
 			new FeatureRenamingCommand(fmManager, modelAlias + oldEvent.getOldValue().toString(), modelAlias + oldEvent.getNewValue().toString()).execute();
@@ -1166,6 +1167,11 @@ public class FeatureDiagramEditor extends FeatureModelEditorPage implements GUID
 			new CreateGraphicalSiblingOperation(graphicalFeatureModel, selectedFeatureName, siblingName).execute();
 			break;
 		case FEATURE_DELETE:
+			// Extract the name of the deleted feature, and rewrite it for this model.
+			final IFeature originalFeatureToDelete = ((IFeature) oldEvent.getSource());
+			final String featureToDeleteName = modelAlias + originalFeatureToDelete.getName();
+			new DeleteFeatureOperation(fmManager, featureToDeleteName).execute();
+			break;
 		case CONSTRAINT_MODIFY:
 			Node newFormula;
 			// For an modified constraint, extract the old constraint description, and find the constraint to replace.
