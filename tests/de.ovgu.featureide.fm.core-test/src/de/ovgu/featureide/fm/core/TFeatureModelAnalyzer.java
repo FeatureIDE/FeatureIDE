e4b24390ad813f7d3fd900831138f2aa3bc8ce20
rahel <rahel.arens@web.de>
2021-11-15 12:49:48 +0100

    Merge conflict in TFeatureModelAnalyzer

--- tests/de.ovgu.featureide.fm.core-test/src/de/ovgu/featureide/fm/core/TFeatureModelAnalyzer.java
+++ tests/de.ovgu.featureide.fm.core-test/src/de/ovgu/featureide/fm/core/TFeatureModelAnalyzer.java
@@ -316,8 +316,4 @@ public class TFeatureModelAnalyzer {
 	public void TDeadFeatures_FM10() {
 		assertTrue(!FM10_DATA.getFeatureModelProperties().hasDeadFeatures());
 	}
-<<<<<<< HEAD
-=======
-
->>>>>>> refs/heads/develop
 }
