11ba7f2a1464348374adce64a1992f145771417b
rahel <rahel.arens@web.de>
2021-11-23 17:18:04 +0100

    Small change to make the root feature of a submodel editable

--- plugins/de.ovgu.featureide.fm.ui/src/de/ovgu/featureide/fm/ui/editors/featuremodel/editparts/ConnectionEditPart.java
+++ plugins/de.ovgu.featureide.fm.ui/src/de/ovgu/featureide/fm/ui/editors/featuremodel/editparts/ConnectionEditPart.java
@@ -152,7 +152,8 @@ public class ConnectionEditPart extends AbstractConnectionEditPart implements GU
 						final Point requestLocation = ((SelectionRequest) request).getLocation();
 						((CircleDecoration) child).translateToRelative(requestLocation);
 						if (decoratorBounds.contains(requestLocation)) {
-							if (!(feature instanceof MultiFeature) || !((MultiFeature) feature).isFromExtern()) {
+							if (!(feature instanceof MultiFeature) || !(((MultiFeature) feature).isFromExtern()
+								&& ((MultiFeature) ((MultiFeature) feature).getStructure().getParent().getFeature()).isFromExtern())) {
 								if (FeatureModelOperationWrapper
 										.run(new MandatoryFeatureOperation(feature.getName(), FeatureModelManager.getInstance(feature.getFeatureModel())))) {
 									return true;
