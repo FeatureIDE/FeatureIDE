53e64c61efae1bf57630fb244659f2ed309c5cd6
Chico Sundermann <chico.sundermann@uni-ulm.de>
2022-08-18 12:24:39 +0200

    Fixes #1239

--- plugins/de.ovgu.featureide.fm.ui/src/de/ovgu/featureide/fm/ui/editors/FeatureModelTextEditorPage.java
+++ plugins/de.ovgu.featureide.fm.ui/src/de/ovgu/featureide/fm/ui/editors/FeatureModelTextEditorPage.java
@@ -124,13 +124,12 @@ public class FeatureModelTextEditorPage extends TextEditor implements IFeatureMo
 		final ProblemList problems = featureModelEditor.checkModel(getCurrentContent());
 		if (problems.containsError()) {
 			createMarkers(problems);
-			throw new RuntimeException();
 		} else {
 			deleteMarkers(getDocumentProvider().getAnnotationModel(getEditorInput()));
-			super.doSave(progressMonitor);
-			executeSaveOperation();
-			featureModelEditor.setPageModified(false);
 		}
+		super.doSave(progressMonitor);
+		executeSaveOperation();
+		featureModelEditor.setPageModified(false);
 	}
 
 	@Override
