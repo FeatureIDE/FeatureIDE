65d325ed96bf68975d3160c4b6de553a3d1c7f35
rahel <rahel.arens@web.de>
2022-08-19 11:34:55 +0200

    Fixes #1102
    * removed Code that synchronizes the constraints in the view and the
    editor
    * added action to show/hide the constraints in the fm editor
    * show constraints property can be saved

--- plugins/de.ovgu.featureide.fm.core/src/de/ovgu/featureide/fm/core/localization/StringTable.java
+++ plugins/de.ovgu.featureide.fm.core/src/de/ovgu/featureide/fm/core/localization/StringTable.java
@@ -1175,4 +1175,5 @@ public class StringTable {
 	public static final String SHOW_ALL_LEVELS = "Show All Levels";
 	public static final String SHA_256_DIGEST_ALGORITHM = "SHA-256";
 	public static final String DELETE_SUBMODEL = "Delete Submodel";
+	public static final String SHOW_CONSTRAINTS = "Show Constraints";
 }
